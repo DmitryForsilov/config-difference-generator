ee72e0da3bb710b6f6c59bc1bb5d9ee85faaa730
DmitryForsilov <tpadtpad@yandex.ru>
2020-03-17 13:55:16 +0300

    add  arg and option to gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,6 +5,13 @@ import commander from 'commander';
 const program = new commander.Command();
 
 program
-  .version('1.0.0')
+  .version('1.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .parse(process.argv);
+  .option('-f, --format [type]', 'output format');
+
+program.arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+
+  });
+
+program.parse(process.argv);
