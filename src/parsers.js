0d65890f40dfa5754f98bbdf49a7335f99f85ba8
DmitryForsilov <tpadtpad@yandex.ru>
2020-03-22 15:25:56 +0300

    refactoring parsers

--- src/parsers.js
+++ src/parsers.js
@@ -3,23 +3,19 @@ import fs from 'fs';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
+const parsersList = {
+  '.json': JSON.parse,
+  '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
+};
+
 const parsers = (fileName) => {
   const absolutePath = path.resolve(process.cwd(), '__fixtures__', fileName);
   const format = path.extname(absolutePath);
   const data = fs.readFileSync(absolutePath, 'utf-8');
+  const parser = parsersList[format];
 
-  switch (format) {
-    case '.json':
-      return JSON.parse(data);
-    case '.yml':
-      return yaml.safeLoad(data);
-    case '.ini':
-      return ini.parse(data);
-    default:
-      break;
-  }
-
-  return `${fileName} file has unsupported format`;
+  return parser(data);
 };
 
 export default parsers;
