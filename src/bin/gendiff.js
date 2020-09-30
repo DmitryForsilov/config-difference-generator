e0070424d67924c9d9cb64f8b5dc2b8ce7600144
DmitryForsilov <tpadtpad@yandex.ru>
2020-09-30 12:14:17 +0300

    fix tests

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,7 +3,7 @@ import program from 'commander';
 import makeDiff from '../index.js';
 
 program
-  .version('0.7.8')
+  .version('0.7.9')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format: default, plain or json', 'default')
   .arguments('<firstConfig> <secondConfig>')
