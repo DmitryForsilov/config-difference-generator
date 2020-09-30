8c9a33ae14026a93adaef00878c5d562da6b9611
DmitryForsilov <tpadtpad@yandex.ru>
2020-09-30 12:13:43 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,12 +5,12 @@ import makeDiff from '../src/index.js';
 const getResultPath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 const getFilePath = (fileName) => path.join('__fixtures__', fileName);
 
-let resultTree;
+let resultDefault;
 let resultPlain;
 let resultJson;
 
 beforeAll(() => {
-  resultTree = fs.readFileSync(getResultPath('resultTree.txt'), 'utf-8');
+  resultDefault = fs.readFileSync(getResultPath('resultDefault.txt'), 'utf-8');
   resultPlain = fs.readFileSync(getResultPath('resultPlain.txt'), 'utf-8');
   resultJson = fs.readFileSync(getResultPath('resultJson.json'), 'utf-8');
 });
@@ -20,7 +20,7 @@ test.each([
   [getFilePath('before.yml'), getFilePath('after.yml')],
   [getFilePath('before.ini'), getFilePath('after.ini')],
 ])('makeDiff', (before, after) => {
-  expect(makeDiff(before, after, 'tree')).toBe(resultTree);
+  expect(makeDiff(before, after, 'default')).toBe(resultDefault);
   expect(makeDiff(before, after, 'plain')).toBe(resultPlain);
   expect((makeDiff(before, after, 'json'))).toBe((resultJson));
 });
